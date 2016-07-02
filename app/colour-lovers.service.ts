b09917f1f1efcff607f1dde3eb64c272c1a4ef19
Neill Bogie <neillbogie@googlemail.com>
2016-07-02 17:58:16 +0100

    Use URLSearchParams to form the URL for ColourLovers API calls.  +Docs.

--- app/colour-lovers.service.ts
+++ app/colour-lovers.service.ts
@@ -8,19 +8,39 @@ export class ColourLoversService {
     constructor(private jsonp: Jsonp) {}
 
     getRandomPalette():Observable<any>{    
-        return this.jsonp.get('http://www.colourlovers.com/api/palettes/random?format=json&showPaletteWidths=1&jsonCallback=JSONP_CALLBACK')
-                    .map(this.extractData)
-                    .catch(this.handleError);    
+
+        let params = new URLSearchParams();
+        params.set('format', 'json');
+        params.set('jsonCallback', 'JSONP_CALLBACK');
+        params.set('showPaletteWidths', '1');
+
+        return this.jsonp
+                .get('http://www.colourlovers.com/api/palettes/random', {search: params})
+                .map(this.extractData)
+                .catch(this.handleError);    
     }
-    getPalette(id: number):Observable<any>{        
-        return this.jsonp.get('http://www.colourlovers.com/api/palette/' + id + '?format=json&showPaletteWidths=1&jsonCallback=JSONP_CALLBACK')
-                    .map(this.extractData)
-                    .catch(this.handleError);    
+    getPalette(id: number):Observable<any>{ 
+    
+        let params = new URLSearchParams();
+        params.set('format', 'json');
+        params.set('jsonCallback', 'JSONP_CALLBACK');
+        params.set('showPaletteWidths', '1');
+
+        return this.jsonp
+                .get('http://www.colourlovers.com/api/palette/' + id, {search: params})
+                .map(this.extractData)
+                .catch(this.handleError);    
     }
+
     getRandomPattern():Observable<any>{
-        return this.jsonp.get('http://www.colourlovers.com/api/patterns/random?format=json&jsonCallback=JSONP_CALLBACK')
-                    .map(this.extractData)
-                    .catch(this.handleError);        
+        let params = new URLSearchParams();
+        params.set('format', 'json');
+        params.set('jsonCallback', 'JSONP_CALLBACK');
+
+        return this.jsonp
+                .get('http://www.colourlovers.com/api/patterns/random', {search: params})
+                .map(this.extractData)
+                .catch(this.handleError);        
     }
 
 
@@ -31,8 +51,24 @@ export class ColourLoversService {
     private handleError (error: any) {
         let errMsg = (error.message) ? error.message :
             error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-        console.error("handleError() got " + JSON.stringify(errMsg));
+        console.error("ColourLoversService#handleError() got " + JSON.stringify(errMsg));
         return Observable.throw(errMsg);
     }
 }
 
+    
+
+
+/**
+ * Notes: 
+ *
+ * ColourLovers API requires the callback parameter be named jsonCallback.
+
+ * From https://angular.io/docs/ts/latest/guide/server-communication.html
+ *    we note that:
+ * 
+ * The JSONP technique requires that we pass a callback function name to
+ * the server in the query string: callback=JSONP_CALLBACK. 
+ * The server uses that name to build a JavaScript wrapper function in 
+ * its response which Angular ultimately calls to extract the data. 
+*/
