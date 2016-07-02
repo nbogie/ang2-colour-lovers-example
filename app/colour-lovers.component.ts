5b2c3b7ece845c957279888b01a5dc72c40812af
Neill Bogie <neillbogie@googlemail.com>
2016-07-02 17:17:37 +0100

    move rxjs imports out to app/rxjs-operators.js

--- app/colour-lovers.component.ts
+++ app/colour-lovers.component.ts
@@ -1,18 +1,7 @@
 import { Component, Input,OnInit } from '@angular/core';
 import { Observable }     from 'rxjs/Observable';
 import { Jsonp, Response, URLSearchParams } from '@angular/http';
-
-// Statics
-import 'rxjs/add/observable/throw';
-
-// Operators
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/toPromise';
-
+import './rxjs-operators';
 
 
 @Component({
